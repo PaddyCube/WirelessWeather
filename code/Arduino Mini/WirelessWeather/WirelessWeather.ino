db557e24dfca0fdcddc90e00d633e310ef737a83
PaddyCube <paddy-daun@web.de>
2019-12-12 21:05:58 +0100

    small changes

--- code/Arduino Mini/WirelessWeather/WirelessWeather.ino
+++ code/Arduino Mini/WirelessWeather/WirelessWeather.ino
@@ -1,18 +1,18 @@
-#include <Adafruit_BME280.h>
+#include <Adafruit_BMP280.h>
 #include <Wire.h>
 #include "WindVane.h"
 #include "settings.h"
-#include <LowPower.h>
+#include "LowPower.h"
 #include <RH_ASK.h> // RadioHead
 #include <SPI.h>    // maybe not needed
 
 WindVane windvane;
-Adafruit_BME280 bme;
+Adafruit_BMP280 bme;
 RH_ASK radioHead;
 
-char temp[6];
-char pressure[6];
-char humidity[6];
+char temp[10];
+char pressure[10];
+char humidity[10];
 char windvalue[10];
 char rainvalue[10];
 char windpos[3];
@@ -26,24 +26,23 @@ volatile unsigned long WakeupCount = 0;
 void setup()
 {
 
-  Serial.begin(115200);
+  Serial.begin(19200);
 
+  Serial.println("Initialize Wireless Weather on Arduino Pro Mini");
   // init I2C communication
-  Wire.begin();                    // join i2c bus (address optional for master)
-  Wire.setClockStretchLimit(1500); // for some reason needed for ATTINY85
-  windvane.begin();
-  Serial.println(bme.begin(0x76));
-  Wire.setClockStretchLimit(1500); // for some reason needed for ATTINY85
-  Serial.println("Setup");
 
   // enable interrupts for wind and rain count
   pinMode(WindPin, INPUT);
   pinMode(RainPin, INPUT);
+  pinMode(SendPower, OUTPUT);
+  pinMode(i2cPower, OUTPUT);
 
   // prepare send data over 433MHz
   radioHead = RH_ASK(2000, 11, SendPin, false);
   if (!radioHead.init())
     Serial.println("433MHz init failed");
+
+  delay(100);
 }
 
 void loop()
@@ -53,17 +52,29 @@ void loop()
   float lv_humidity;
   unsigned int lv_wind;
   unsigned int lv_rain;
-  unsigned int lv_battery;
-  unsigned int lv_solar;
+  float lv_battery;
+  float lv_solar;
 
   // check if we need to send data
   if (WakeupCount < (SLEEP_MIN * 60 / 8))
   {
     // no data needs to be send, go back to sleep
-    Serial.println("nothing to do, go back to sleep");
   }
   else
   {
+
+    digitalWrite(SendPin, HIGH);
+    digitalWrite(i2cPower, HIGH);
+    delay(50);
+
+    Wire.begin();                    // join i2c bus (address optional for master)
+    // Wire.setClockStretchLimit(1500); // for some reason needed for ATTINY85
+    windvane.begin();
+    bme.begin(0x76);
+    //  Wire.setClockStretchLimit(1500); // for some reason needed for ATTINY85
+
+
+    WakeupCount = 0;
     // data needs to be send
     snprintf(msg, 50, "Alive since %ld milliseconds", millis());
     Serial.print("Publish message: ");
@@ -71,12 +82,15 @@ void loop()
 
     // Read data from BME280
     lv_temp = bme.readTemperature();
+
     lv_pressure = bme.readPressure();
-    lv_humidity = bme.readHumidity();
+    //    lv_humidity = bme.readHumidity();
 
     // convert results to char
     dtostrf(lv_temp, 6, 2, temp);
+    Serial.println(temp);
     dtostrf(lv_pressure, 6, 2, pressure);
+    Serial.println(pressure);
     dtostrf(lv_humidity, 6, 2, humidity);
 
     // send Wind speed and rain count
@@ -87,61 +101,61 @@ void loop()
 
     // battery level
     lv_battery = measureBattery();
-    itoa(lv_battery, batvalue, 10);
+    itoa(lv_battery * 100, batvalue, 10);
 
     // solar
     lv_solar = measureSolar();
-    itoa(lv_solar, solar, 10);
+    itoa(lv_solar * 100, solar, 10);
 
     //send data over 433 MHz
-    msg = "WW" + temp + '|' + pressure + '|' + humidity + '|' + windvalue + '|' + rainvalue + '|' +
-          windpos + '|' + batvalue + '|' + solar;
+   // sprintf(msg, "%s %s", "WW", temp);
 
+    sprintf(msg, "%s|%s|%s|%s|%s|%s|%s|%s|%s|END", "WW", temp, pressure, humidity,windvalue,rainvalue,windpos,batvalue,solar);
+    Serial.println(msg);
+    
     radioHead.send((uint8_t *)msg, strlen(msg));
 
     // reset wind and rain counts
     windcount = 0;
     raincount = 0;
+
+    delay(100); // wait to complete send
+
+    digitalWrite(SendPin, LOW); // turn off external devices
+    digitalWrite(i2cPower, LOW);
   }
 
   // attach Interrupts
   attachInterrupt(digitalPinToInterrupt(WindPin), onWindPulse, FALLING);
   attachInterrupt(digitalPinToInterrupt(RainPin), onWindPulse, FALLING);
 
+
   //   Sleep for x Minute
-  LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
+  LowPower.powerDown(SLEEP_1S, ADC_OFF, BOD_OFF);
 
   // back from sleep
-  detachInterrupt();
+  detachInterrupt(digitalPinToInterrupt(WindPin));
+  detachInterrupt(digitalPinToInterrupt(RainPin));
   onTimerWake();
 }
 
 float measureSolar()
 {
 
-  // Voltage divider R1 = 220k+100k+220k =540k and R2=100k
-  float calib_factor = 18.3; //5.28; // change this value to calibrate the battery voltage
-  unsigned long raw = analogRead(A0);
+  // Voltage divider R1 = 1M and R2=560k
+  float calib_factor = 3.78; // change this value to calibrate the battery voltage
+  unsigned long raw = analogRead(A1);
   float volt = raw * calib_factor / 1024;
-
-  Serial.print(" m. \nVoltage Solar = ");
-  Serial.print(volt, 2); // print with 2 decimal places
-  Serial.println(" V");
-
   return volt;
 }
 
 float measureBattery()
 {
 
-  // Voltage divider R1 = 220k+100k+220k =540k and R2=100k
-  float calib_factor = 18.3; //5.28; // change this value to calibrate the battery voltage
-  unsigned long raw = analogRead(A1);
+  // Voltage divider R1 = 1000k and R2=560k
+  float calib_factor = 9.4; // change this value to calibrate the battery voltage
+  unsigned long raw = analogRead(A0);
   float volt = raw * calib_factor / 1024;
-
-  Serial.print(" m. \nVoltage Battery = ");
-  Serial.print(volt, 2); // print with 2 decimal places
-  Serial.println(" V");
 
   return volt;
 }
@@ -152,7 +166,7 @@ void onWindPulse()
   delay(2); // delay for debounce
 }
 
-void onWindPulse()
+void onRainPulse()
 {
   raincount++;
   delay(2); // delay for debounce
--- code/Arduino Mini/WirelessWeather/settings.h
+++ code/Arduino Mini/WirelessWeather/settings.h
@@ -1,10 +1,13 @@
 // Sleep settings for ESP8266
-const int SLEEP_MIN = 3;
+const int SLEEP_MIN = 1;
 
 // Pins for wind and rain count
 const int WindPin = 2;
 const int RainPin = 3;
 
 // pin for data send
-const int SendPin = 5;
+const int SendPin = 10;
+const int SendPower = 11;
 
+// power for I2C devices
+const int i2cPower = 5;
